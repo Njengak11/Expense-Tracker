3b84c22ace52348d776364472a9bf31b5606a25a
Njengak11 <njengak11@gmail.com>
2021-11-08 12:25:37 +0300

    added actions

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react';
+import GlobalContext from '../context/GlobalState';
 
 const Expense = () => {
+    const {transactions} = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const income = amounts
+      .filter(item => item > 0)
+      .reduce((acc, item) => (acc += item), 0);
+  
+    const expense = (
+      amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+      -1
+    );
 
     return (
         <div className="exp-container">
             <div>
-                <h4>Income</h4>
-                <p  className='money plus'>+ Ksh 0.00</p>
+                <h4>Your Income</h4>
+                <p  className='money plus'>+ Ksh {income}</p>
             </div>
             <div>
-                <h4>Expense</h4>
-                <p  className='money minus'>- Ksh 0.00</p>
+                <h4>Your Expense</h4>
+                <p  className='money minus'>- Ksh {expense}</p>
             </div>
         </div>
     )
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import GlobalContext from '../context/GlobalState';
 
 const Transaction = ({ transaction }) => {
+    const { deleteTransaction} = useContext(GlobalContext);
 
 const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
         {transaction.text} <span>{sign} Ksh {Math.abs(transaction.amount)}</span>
-        <button className='delete-btn'>x</button>
+        <button className='delete-btn'
+        onClick={() => deleteTransaction(transaction.id)}
+        >x</button>
     </li>
     )
 }
